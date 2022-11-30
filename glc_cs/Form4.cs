2725a085746ca0c65f7add2e6605b77f582dc912
deko <fanta51@live.jp>
2022-11-30 18:42:15 +0900

    v1.01

--- glc_cs/Form4.cs
+++ glc_cs/Form4.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Windows.Forms;
 
@@ -32,38 +31,3 @@ namespace glc_cs
 		}
 	}
 }
-=======
-﻿using System;
-using System.Windows.Forms;
-
-namespace glc_cs
-{
-	public partial class Form4 : Form
-	{
-		public Form4(string imgPath)
-		{
-			InitializeComponent();
-			pictureBox1.ImageLocation = imgPath;
-		}
-
-		private void trackBar1_Scroll(object sender, EventArgs e)
-		{
-			if (trackBar1.Value == 0)
-			{
-				pictureBox1.Width = 100;
-				pictureBox1.Height = 100;
-			}
-			else if (trackBar1.Value == 1)
-			{
-				pictureBox1.Width = 200;
-				pictureBox1.Height = 200;
-			}
-			else
-			{
-				pictureBox1.Width = 300;
-				pictureBox1.Height = 300;
-			}
-		}
-	}
-}
->>>>>>> 8a988ffcc8fc1e547c7e4f6982d544e3414ced0b
