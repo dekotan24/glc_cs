3c6a9a83df3e42f49bc78e29fbc96bd4f3c643d8
dekotan24 <master@fanet.work>
2022-11-30 18:17:49 +0900

    v1.01

--- glc_cs/Properties/AssemblyInfo.cs
+++ glc_cs/Properties/AssemblyInfo.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Resources;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -38,44 +37,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("25.22.09.17")]
 [assembly: NeutralResourcesLanguage("ja-JP")]
-=======
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// 制御されます。アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更します。
-
-[assembly: AssemblyTitle("Game Launcher C# Edition")]
-[assembly: AssemblyDescription("Game Launcherでゲームを管理")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("dekosoft")]
-[assembly: AssemblyProduct("Game Launcher C# Edition")]
-[assembly: AssemblyCopyright("(c) 2019-2022 Ogura Deko")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-
-// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
-// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
-// その型の ComVisible 属性を true に設定してください。
-[assembly: ComVisible(false)]
-
-// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("674fe692-e59d-444d-a524-1ae48923e8ab")]
-
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
-//
-//      メジャー バージョン
-//      マイナー バージョン
-//      ビルド番号
-//      リビジョン
-//
-// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
-// 既定値にすることができます:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("25.22.09.17")]
-[assembly: NeutralResourcesLanguage("ja-JP")]
->>>>>>> 8a988ffcc8fc1e547c7e4f6982d544e3414ced0b
