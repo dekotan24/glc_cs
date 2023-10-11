979508f7a28899eb5992d9cbeaa172deb82ea382
Ogura Deko <fanta51@live.jp>
2023-10-12 01:25:08 +0900

    v1.11 sql injection対応

---
+++ glc_cs/AuthData.cs
@@ -0,0 +1,17 @@
+﻿namespace glc_cs
+{
+	internal class AuthData
+	{
+		private readonly string dmmAPI = "hHzqSTpm5ZNmZwn6Fbew";    // DMMのAPIキー
+		private readonly string dmmAffID = "deko-990";              // DMMのアフィリエイトID
+
+		/// <summary>
+		/// DMMのAPIキー
+		/// </summary>
+		protected string GetDmmAPI
+		{
+			get { return dmmAPI; }
+		}
+
+	}
+}
